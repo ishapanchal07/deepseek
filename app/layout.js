9b5d385ab6184484e3b514e6acc0b096e5464406
ishapanchal07 <isha7112003@gmail.com>
2025-12-25 18:27:10 +0530

    signin page added

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inder } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inder = Inder({
   subsets: ["latin"],
@@ -14,10 +15,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body className={`${inder.className} antialiased`}>
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
